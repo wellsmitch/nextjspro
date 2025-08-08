818a6127aaec55c084cbe7a76b1534ed8bd33809
songyubnin <123@example.com>
2025-08-08 11:42:55 +0800

    FloatButton 添加置顶

--- app/blog/clientPage.tsx
+++ app/blog/clientPage.tsx
@@ -1,16 +1,22 @@
 "use client"
 import React, { Suspense } from 'react';
-import { Breadcrumb, ConfigProvider, Layout, Menu, MenuProps, Space, theme, Button, Alert } from 'antd';
+import {
+  Breadcrumb,
+  Image as AntdImage,
+  ConfigProvider, Layout, Menu, MenuProps, Space, theme, Button, Alert, Affix, FloatButton, Typography, Popover
+} from 'antd';
 
 import CategoryList from '../components/pc/CategoryList';
 import Cheader from "@/app/blog/CustomHeader"
 import "./index.scss"
 import Marquee from 'react-fast-marquee';
+import { MailOutlined, WechatFilled } from '@ant-design/icons';
 const { Content, Footer } = Layout;
 const App: React.FC = (props) => {
   const {
     token: { colorBgContainer, borderRadiusLG },
   } = theme.useToken();
+  const [containerRef, setContainer] = React.useState<HTMLDivElement | null>(null);
 
   return (
     // <ConfigProvider>
@@ -26,7 +32,9 @@ const App: React.FC = (props) => {
           </Marquee>
         }
       />
-      <Content style={{ padding: '0 48px' }}>
+      <Content style={{ padding: '0 48px' }} ref={(d: HTMLDivElement) => {
+        d && setContainer(d)
+      }}>
 
         <div
 
@@ -40,6 +48,29 @@ const App: React.FC = (props) => {
           <CategoryList />
         </div>
       </Content>
+      <FloatButton.Group shape="circle">
+        <FloatButton.BackTop duration={500} visibilityHeight={500} />
+        <Popover placement="left" content={() => {
+
+          return <>
+            <Typography.Paragraph style={{ margin: 0 }} copyable >sybabjj@163.com</Typography.Paragraph>
+          </>
+        }}>
+          <FloatButton badge={{ dot: true }} icon={<MailOutlined />} />
+        </Popover>
+
+        <Popover placement="left" content={() => {
+
+          return <>
+            <p style={{ textAlign: "center" }}>客官，来个赏吧~</p>
+            <AntdImage style={{ width: 150 }} src={"/passets/images/allowpay.jpg"} />
+          </>
+        }}>
+          <FloatButton badge={{ dot: true }} icon={<WechatFilled />} />
+        </Popover>
+
+
+      </FloatButton.Group>
       <Footer style={{ textAlign: 'center' }}>
         <Button type='link' href='https://beian.miit.gov.cn/' target='_blank'>豫ICP备19041297号</Button>{new Date().getFullYear()} Blog Web
       </Footer>
