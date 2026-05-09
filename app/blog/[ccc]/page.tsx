e521756fa00681aa653a336a0929a74b9c9eb672
宋玉彬 <songyb09@chinatelecom.cn>
2026-05-09 11:23:04 +0800

    f

--- app/blog/[ccc]/page.tsx
+++ app/blog/[ccc]/page.tsx
@@ -1,6 +1,7 @@
 
 import type { Metadata } from "next";
 // import CccClient from "./ClientCpt"
+import Custom from "./Custom";
 import fs from "fs"
 import path from "path"
 
@@ -9,15 +10,26 @@ export const metadata: Metadata = {
   title: 'daa',
   description: '...',
 }
-export default  function Page() {
- // 读取本地文件
+export default function Page() {
+  // 读取本地文件
   const data = fs.readFileSync(path.join(process.cwd(), 'ddd.json'), 'utf8');
   console.log(data, "<<<<<<<<<<<<<<<<<<");
-  
- return (
-  <>
-  {/* <CccClient/> */}
-   123aaa {data}
-  </>
- )
+  const doSave = async () => {
+    // return async () => {
+      const data = { name: 'Next.js', version: Math.random() };
+      await fs.writeFile(path.join(process.cwd(), 'ddd.json'), JSON.stringify(data, null, 2), null, ()=>{});
+
+    // }
+  }
+setTimeout(()=> {
+  doSave()
+}, 2000)
+  return (
+    <>
+      {/* <CccClient/> */}
+      123aaa {data}
+      {/* <Custom doSave={doSave()} /> */}
+      {/* <button onClick={()=>doSave}>post</button> */}
+    </>
+  )
 }
