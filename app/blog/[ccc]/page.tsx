e43b3bc3e19a38f6f44423d1d5d47c90686679cb
宋玉彬 <songyb09@chinatelecom.cn>
2026-05-09 10:39:41 +0800

    feat: 文件json 测试

--- app/blog/[ccc]/page.tsx
+++ app/blog/[ccc]/page.tsx
@@ -1,6 +1,8 @@
 
 import type { Metadata } from "next";
 // import CccClient from "./ClientCpt"
+import fs from "fs"
+import path from "path"
 
 
 export const metadata: Metadata = {
@@ -8,11 +10,14 @@ export const metadata: Metadata = {
   description: '...',
 }
 export default  function Page() {
-
+ // 读取本地文件
+  const data = fs.readFileSync(path.join(process.cwd(), 'ddd.json'), 'utf8');
+  console.log(data, "<<<<<<<<<<<<<<<<<<");
+  
  return (
   <>
   {/* <CccClient/> */}
-   123aaa
+   123aaa {data}
   </>
  )
 }
