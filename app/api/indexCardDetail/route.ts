839862f1e22cd7c780ca7e17698cba30158cabb4
宋玉彬 <songyb09@chinatelecom.cn>
2026-07-30 14:06:15 +0800

    force-dynamic

--- app/api/indexCardDetail/route.ts
+++ app/api/indexCardDetail/route.ts
@@ -1,40 +1,41 @@
+// app/api/indexCardDetail/route.ts
+export const dynamic = 'force-dynamic';  // 强制动态渲染
 // 后端接口 上层可以有文件夹  不能 和 page.tsx 同一层目录
-import Network from "@/network"
 import { NextResponse, NextRequest } from "next/server"
 import path from "path";
 import fs from "fs";
 
 export async function GET(request: NextRequest) {
  try {
-//   const res = await Network.get("", {
-//    params: {
-//     "tableName": "codeList",
-//    }
-//   })
-const info  = request.nextUrl.searchParams
-const code = info.get("code")
-const like = info.get("like")
-    // 读取 JSON 文件
-const filePath = path.join("/tmp", 'indexData.json');
-const fileContent = fs.readFileSync(filePath, 'utf8');
+    //   const res = await Network.get("", {
+    //    params: {
+    //     "tableName": "codeList",
+    //    }
+    //   })
+    const info  = request.nextUrl.searchParams
+    const code = info.get("code")
+    const like = info.get("like")
+        // 读取 JSON 文件
+    const filePath = path.join("/tmp", 'indexData.json');
+    const fileContent = fs.readFileSync(filePath, 'utf8');
 
-// 将 JSON 字符串解析为 JavaScript 对象
-const jsonData = JSON.parse(fileContent) as [];
-  return NextResponse.json({ 
-    errMsg: null,  
-    results: jsonData.filter((info: any)=> {
-      if(like) {
-        return info.moduleType ===like
-      }
-      return (
-        info.moduleType ===code
-      )
-      })
- })
+    // 将 JSON 字符串解析为 JavaScript 对象
+    const jsonData = JSON.parse(fileContent) as [];
+      return NextResponse.json({ 
+        errMsg: null,  
+        results: jsonData.filter((info: any)=> {
+          if(like) {
+            return info.moduleType ===like
+          }
+          return (
+            info.moduleType ===code
+          )
+          })
+    })
  } catch (err: any) {
-  console.log('err', err)
-  return NextResponse.json({
-   errMsg: err?.message || ""
-  })
+    console.log('err', err)
+    return NextResponse.json({
+    errMsg: err?.message || ""
+    })
  }
 }
